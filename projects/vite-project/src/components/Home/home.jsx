5a10ce54d3743464b184b5156ed8fcad13a57c21
Cicistem <ceciliarosalese04@gmail.com>
2023-08-08 17:48:20 -0300

    creacion del proyecto se agregan y eliminan carpetas iniciales

---
+++ projects/vite-project/src/components/Home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar';
+
+const Home = () => {
+  return (
+    <> //Fragments
+    <Navbar/>
+  <h1>Home</h1>
+    </>
+  );
+  
+};
+
+export default Home;
---
+++ projects/vite-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <nav className='navbar'></nav>
+        <h1 className='navbar-logo'>Autoescuela Giachello</h1>
+      
+    </div>
+  );
+};
+
+export default Navbar;
